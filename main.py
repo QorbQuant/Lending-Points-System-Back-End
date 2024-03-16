119caf385aa1ca09ab4512b5089834075e84ee40
QorbQuant <qorbanferrell@gmail.com>
2024-03-16 10:10:40 +0700

    adjust insert logic

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from contracts import CONTRACT_ADDRESSES
 import pandas as pd
 import requests
 
-# Main workflow
-load_dotenv()  # Loads the environment variables from .env file
+load_dotenv()
 api_key = os.getenv('LINEA_API_KEY')
 address = CONTRACT_ADDRESSES["deposit_contract"]
-db_conn_str = os.getenv('db_conn_str')  # Assuming you have your database URL in the .env file
+db_conn_str = os.getenv('db_conn_str')
 
 
 def fetch_transactions(address, api_key):
+    """fetch transactions utilizing linea API"""
+
     base_url = "https://api.lineascan.build/api"
     params = {
         'module': 'account',
@@ -21,7 +22,7 @@ def fetch_transactions(address, api_key):
         'startblock': 0,
         'endblock': 'latest',
         'page': 1,
-        'offset': 100,  # Adjust based on API limits
+        'offset': 100,
         'sort': 'asc',
         'apikey': api_key
     }
@@ -33,10 +34,10 @@ def fetch_transactions(address, api_key):
         if response.status_code == 200:
             data = response.json()
             results = data.get('result', [])
-            if not results:  # No more results
+            if not results:
                 break
             transactions.extend(results)
-            params['page'] += 1  # Go to the next page
+            params['page'] += 1
         else:
             print("Failed to fetch data from the API. Status code:", response.status_code)
             break
@@ -45,20 +46,31 @@ def fetch_transactions(address, api_key):
 
 
 def insert_transactions_to_db(grouped_data_sorted, db_conn_str):
-    # Connect to your database
+    """connect to db and insert data"""
+
     conn = psycopg2.connect(db_conn_str)
     cur = conn.cursor()
 
-    # Assuming you have a table created like this:
-    # CREATE TABLE transaction_counts (address VARCHAR(255), hash_count INT, points INT);
+    # Clear the table before inserting new data
+    try:
+        cur.execute("DELETE FROM transaction_counts")
+        conn.commit()
+    except Exception as e:
+        print("An error occurred while trying to delete data: ", e)
+        conn.rollback()  # Rollback the transaction on error
 
-    for _, row in grouped_data_sorted.iterrows():
-        cur.execute(
-            "INSERT INTO transaction_counts (address, hash_count, points) VALUES (%s, %s, %s)",
-            (row['from'], row['hash_count'], row['points'])
-        )
+    # Now, insert the new data
+    try:
+        for _, row in grouped_data_sorted.iterrows():
+            cur.execute(
+                "INSERT INTO transaction_counts (address, hash_count, points) VALUES (%s, %s, %s)",
+                (row['from'], row['hash_count'], row['points'])
+            )
+        conn.commit()  # Commit the transaction
+    except Exception as e:
+        print("An error occurred while trying to insert data: ", e)
+        conn.rollback()  # Rollback the transaction on error
 
-    conn.commit()  # Commit the transaction
     cur.close()
     conn.close()
 
@@ -76,8 +88,8 @@ if all_transactions:
 
     # Display the final DataFrame
     insert_transactions_to_db(grouped_data_sorted, db_conn_str)
+    print("transactions inserted into db")
 else:
     print("No transactions found.")
 
-# if __name__ == '__main__':
-#     print(grouped_data_sorted)
+
